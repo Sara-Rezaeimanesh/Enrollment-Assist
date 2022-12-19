6df2f70538db8ef1610e54d4b8e7a54ed226ce16
Sara <Sara.rezaeimanesh2000@gmail.com>
2022-12-19 16:34:06 +0330

    Added CourseControllerTest.java

--- src/test/java/ir/proprog/enrollassist/controller/course/CourseControllerTest.java
+++ src/test/java/ir/proprog/enrollassist/controller/course/CourseControllerTest.java
@@ -1,21 +1,115 @@
 package ir.proprog.enrollassist.controller.course;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ir.proprog.enrollassist.Exception.ExceptionList;
+import ir.proprog.enrollassist.domain.course.AddCourseService;
+import ir.proprog.enrollassist.domain.course.Course;
+import ir.proprog.enrollassist.repository.CourseRepository;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.util.*;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.server.ResponseStatusException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
+@WebMvcTest(CourseController.class)
 class CourseControllerTest {
-
     @Autowired
-    private CourseController controller;
+    private MockMvc mockMvc;
+    @MockBean
+    private CourseRepository cr;
+    @MockBean
+    private AddCourseService acs;
+    private JacksonTester<CourseMajorView> jt;
+    private CourseMajorView cmw;
 
-    @Test
-    public void contextLoads() throws Exception {
-        assertThat(controller).isNotNull();
+    @BeforeEach
+    public void setup() throws ExceptionList {
+        Course c = new Course("9234567", "AD", 4, "Masters");
+        cmw = new CourseMajorView(c, new HashSet<Long>(), new HashSet<Long>());
+        ArrayList<Course> courses = new ArrayList<>() {{
+            add(new Course("1234567", "AI", 4, "Masters"));
+            add(new Course("1234568", "DL", 4, "Masters"));
+            add(new Course("1234569", "NLP", 4, "Masters"));
+        }};
+        given(this.cr.findById(1L)).willReturn(Optional.ofNullable(courses.get(1)));
+        given(this.cr.findAll()).willReturn(courses);
+        given(this.acs.addCourse(any())).willReturn(c);
+
+        JacksonTester.initFields(this, new ObjectMapper());
     }
 
+    @Test
+    public void allWithNoCoursesShouldReturnNoCourseViews() throws Exception {
+        given(this.cr.findAll()).willReturn(new ArrayList<Course>());
+        mockMvc.perform(get("/courses"))
+                .andExpect(jsonPath("$[*]").isEmpty())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void allWithThreeCoursesShouldReturnAllCourseViews() throws Exception {
+        mockMvc.perform(get("/courses"))
+                .andExpect(jsonPath("$[*].courseTitle", containsInAnyOrder("AI", "DL", "NLP")))
+                .andExpect(jsonPath("$[*].graduateLevel", containsInAnyOrder("Masters", "Masters", "Masters")))
+                .andExpect(jsonPath("$[*].courseNumber.courseNumber", containsInAnyOrder("1234568", "1234567", "1234569")))
+                .andExpect(jsonPath("$[*].courseCredits", containsInAnyOrder(4, 4, 4)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void oneShouldReturnCourseViewNumberOne() throws Exception {
+        mockMvc.perform(get("/courses/1"))
+                .andExpect(jsonPath("$.courseNumber.courseNumber").value("1234568"))
+                .andExpect(jsonPath("$.courseTitle").value("DL"))
+                .andExpect(jsonPath("$.graduateLevel").value("Masters"))
+                .andExpect(jsonPath("$.courseCredits").value(4))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void oneWithInvalidIdShouldReturnResponseStatusException() throws Exception {
+        mockMvc.perform(get("/courses/3"))
+                .andExpect(status().reason(containsString("Course not found")))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void addNewCourseShouldReturnCourseView() throws Exception {
+        mockMvc.perform(post("/courses/").contentType(MediaType.APPLICATION_JSON)
+                .content(jt.write(cmw).getJson()))
+                .andExpect(jsonPath("$.courseNumber.courseNumber").value("9234567"))
+                .andExpect(jsonPath("$.courseTitle").value("AD"))
+                .andExpect(jsonPath("$.graduateLevel").value("Masters"))
+                .andExpect(jsonPath("$.courseCredits").value(4))
+                .andExpect(status().isOk());
+    }
 }
