c7ca9a4a0b0f6af6d9daf6db2e68019a2f7a11bf
Sara <Sara.rezaeimanesh2000@gmail.com>
2022-12-30 11:32:52 +0330

    CheckValidGPALimitTest.java and CheckExamTimeConflictsTest.java parametrized tests updated to run with jupiter.

--- src/test/java/ir/proprog/enrollassist/domain/enrollmentList/CheckExamTimeConflictsTest.java
+++ src/test/java/ir/proprog/enrollassist/domain/enrollmentList/CheckExamTimeConflictsTest.java
@@ -1,77 +1,23 @@
 package ir.proprog.enrollassist.domain.enrollmentList;
-
-import ir.proprog.enrollassist.Exception.ExceptionList;
 import ir.proprog.enrollassist.domain.course.Course;
 import ir.proprog.enrollassist.domain.section.ExamTime;
 import ir.proprog.enrollassist.domain.section.Section;
 import ir.proprog.enrollassist.domain.student.Student;
-import lombok.NonNull;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
+import java.util.stream.Stream;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-//import static org.junit.jupiter.api.AssertTrue.assertTrue;
 
-@RunWith(Parameterized.class)
+
 public class CheckExamTimeConflictsTest {
 
-    private EnrollmentList list;
-    private Student stu;
-    private List<Course> courses;
-    private List<Section> sections;
-
-    private List<String> times;
-    private int numErr;
-
-    public CheckExamTimeConflictsTest(List<Integer> sec_numbers, int numSec,int numError) throws Exception {
-
-        courses = new ArrayList<>(){{
-            add(new Course("1234567", "AI", 4, "Masters"));
-            add(new Course("1234568", "DL", 4, "Masters"));
-            add(new Course("1234569", "NLP", 4, "Masters"));
-        }};
-        sections = new ArrayList<>();
-        sections.add(new Section(courses.get(0), "1"));//null
-        sections.add(new Section(courses.get(1), "2"));
-        sections.add(new Section(courses.get(2), "3"));
-
-        times = new ArrayList<>(){{
-            add("2021-06-21T08:00");//0
-            add("2021-06-21T09:00");//1
-            add("2021-06-21T10:00");//2
-            add("2021-06-21T11:00");//3
-            add("2021-06-21T12:00");//4
-            add("2021-06-21T13:00");//5
-            add("2021-06-21T14:00");//6
-            add("2021-06-21T15:00");//7
-            add("2021-06-21T16:00");//8
-            add("2021-06-21T17:00");//9
-            add("2021-06-21T18:00");//10
-            add("2021-06-21T19:00");//11
-            add("2021-06-21T20:00");//12
-        }};
-        stu = new Student("810100000", "Masters");
-        list = new EnrollmentList("12345", stu);
-        for(int i = 0 ; i < numSec ; i++)
-            if(sec_numbers.get(2*i) != -1 || sec_numbers.get(2*i + 1) != -1) {
-                sections.get(i).setExamTime(new ExamTime(times.get(sec_numbers.get(2*i)), times.get(sec_numbers.get(2*i+1))));
-            }
-        numErr = numError;
-        list.addSections(sections.get(0), sections.get(1), sections.get(2));
-    }
-
-    private static List<List<Integer>> sec_params;
-
-    @Parameterized.Parameters
-    public static Collection<Object[]> parameters() {
+    private static Stream<Arguments> parameters() {
+        List<List<Integer>> sec_params;
         sec_params = new ArrayList<>(){{
             add(new ArrayList<>(){{add(-1); add(-1); add(-1); add(-1); add(-1); add(-1);}});//0
             add(new ArrayList<>(){{add(0); add(3); add(-1); add(-1); add(-1); add(-1);}});//1
@@ -94,19 +40,73 @@ public class CheckExamTimeConflictsTest {
             add(new ArrayList<>(){{add(0); add(1); add(2); add(3); add(4); add(5);}});//17 تداخل نداشتن
         }};
 
-        return Arrays.asList(new Object [][]{ {sec_params.get(0), 1, 0}, {sec_params.get(1), 1, 0},
-                {sec_params.get(0), 2, 0}, {sec_params.get(1), 2, 0}, {sec_params.get(2), 2, 0}, {sec_params.get(3), 2, 0},{sec_params.get(4), 2, 1},
-                {sec_params.get(0), 3, 0}, {sec_params.get(5), 3, 0}, {sec_params.get(2), 3, 0},
-                {sec_params.get(6), 3, 1},  {sec_params.get(7), 3, 0}, {sec_params.get(1), 3, 0},
-                {sec_params.get(8), 3, 1}, {sec_params.get(9), 3, 0}, {sec_params.get(3), 3, 0},
-                {sec_params.get(4), 3, 1}, {sec_params.get(10), 3, 1}, {sec_params.get(11), 3, 1},
-                {sec_params.get(12), 3, 1}, {sec_params.get(13), 3, 3}, {sec_params.get(14), 3, 2},
-                {sec_params.get(15), 3, 2}, {sec_params.get(16), 3, 2}, {sec_params.get(17), 3, 0},
-        });
+        return Stream.of(
+                Arguments.of(sec_params.get(0), 1, 0),
+                Arguments.of(sec_params.get(1), 1, 0),
+                Arguments.of(sec_params.get(0), 2, 0),
+                Arguments.of(sec_params.get(1), 2, 0),
+                Arguments.of(sec_params.get(2), 2, 0),
+                Arguments.of(sec_params.get(3), 2, 0),
+                Arguments.of(sec_params.get(4), 2, 1),
+                Arguments.of(sec_params.get(0), 3, 0),
+                Arguments.of(sec_params.get(5), 3, 0),
+                Arguments.of(sec_params.get(2), 3, 0),
+
+                Arguments.of(sec_params.get(6), 3, 1),
+                Arguments.of(sec_params.get(7), 3, 0),
+                Arguments.of(sec_params.get(1), 3, 0),
+                Arguments.of(sec_params.get(1), 2, 0),
+                Arguments.of(sec_params.get(8), 3, 1),
+                Arguments.of(sec_params.get(9), 3, 0),
+                Arguments.of(sec_params.get(3), 3, 0),
+                Arguments.of(sec_params.get(4), 3, 1),
+                Arguments.of(sec_params.get(10), 3, 1),
+                Arguments.of(sec_params.get(11), 3, 1),
+
+                Arguments.of(sec_params.get(12), 3, 1),
+                Arguments.of(sec_params.get(13), 3, 3),
+                Arguments.of(sec_params.get(14), 3, 2),
+                Arguments.of(sec_params.get(15), 3, 2),
+                Arguments.of(sec_params.get(16), 3, 2),
+                Arguments.of(sec_params.get(17), 3, 0)
+        );
     }
 
-    @Test
-    public void checkExamTimeConflictsTest(){
-        assertEquals(list.checkExamTimeConflicts().size(), numErr);
+    @ParameterizedTest
+    @MethodSource("parameters")
+    public void checkExamTimeConflictsTest(List<Integer> sec_numbers, int numSec,int numError) throws Exception {
+        List<Course> courses = new ArrayList<>() {{
+            add(new Course("1234567", "AI", 4, "Masters"));
+            add(new Course("1234568", "DL", 4, "Masters"));
+            add(new Course("1234569", "NLP", 4, "Masters"));
+        }};
+        List<Section> sections = new ArrayList<>();
+        sections.add(new Section(courses.get(0), "1"));//null
+        sections.add(new Section(courses.get(1), "2"));
+        sections.add(new Section(courses.get(2), "3"));
+
+        List<String> times = new ArrayList<>() {{
+            add("2021-06-21T08:00");//0
+            add("2021-06-21T09:00");//1
+            add("2021-06-21T10:00");//2
+            add("2021-06-21T11:00");//3
+            add("2021-06-21T12:00");//4
+            add("2021-06-21T13:00");//5
+            add("2021-06-21T14:00");//6
+            add("2021-06-21T15:00");//7
+            add("2021-06-21T16:00");//8
+            add("2021-06-21T17:00");//9
+            add("2021-06-21T18:00");//10
+            add("2021-06-21T19:00");//11
+            add("2021-06-21T20:00");//12
+        }};
+        Student stu = new Student("810100000", "Masters");
+        EnrollmentList list = new EnrollmentList("12345", stu);
+        for(int i = 0 ; i < numSec ; i++)
+            if(sec_numbers.get(2*i) != -1 || sec_numbers.get(2*i + 1) != -1) {
+                sections.get(i).setExamTime(new ExamTime(times.get(sec_numbers.get(2*i)), times.get(sec_numbers.get(2*i+1))));
+            }
+        list.addSections(sections.get(0), sections.get(1), sections.get(2));
+        Assertions.assertEquals(list.checkExamTimeConflicts().size(), numError);
     }
 }
--- src/test/java/ir/proprog/enrollassist/domain/enrollmentList/CheckValidGPALimitTest.java
+++ src/test/java/ir/proprog/enrollassist/domain/enrollmentList/CheckValidGPALimitTest.java
@@ -1,45 +1,51 @@
 package ir.proprog.enrollassist.domain.enrollmentList;
 
-import ir.proprog.enrollassist.Exception.ExceptionList;
 import ir.proprog.enrollassist.domain.EnrollmentRules.*;
 import ir.proprog.enrollassist.domain.GraduateLevel;
 import ir.proprog.enrollassist.domain.course.Course;
 import ir.proprog.enrollassist.domain.section.Section;
 import ir.proprog.enrollassist.domain.student.Student;
 import ir.proprog.enrollassist.domain.studyRecord.Grade;
-import ir.proprog.enrollassist.domain.studyRecord.StudyRecord;
-import org.junit.experimental.theories.DataPoints;
-import org.junit.experimental.theories.Theories;
-import org.junit.experimental.theories.Theory;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
-
-import static java.lang.Math.min;
-import static org.junit.Assume.assumeTrue;
+import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
-@RunWith(Parameterized.class)
 public class CheckValidGPALimitTest {
-    private final EnrollmentList el;
     private static final int MIN_ERR = 1;
     private static final int MAX_CREDIT_FIRST_SEM = 20;
     private static final int MAX_CREDIT_FAILED = 14;
     private static final int MAX_ERR = 0;
-    private final int errorClass;
-    private final int expected_length;
-    private final ArrayList<Integer> exp_total;
-    public CheckValidGPALimitTest(GraduateLevel gl, double gpa, int totalCredit, int totalCreditTaken, int cls, int errNum, ArrayList<Integer> exp_fail_max) throws Exception {
+
+    private static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of(GraduateLevel.Undergraduate, 20, 10, 0, MIN_ERR, 1, new ArrayList<>(){{add(0);}} ),
+                Arguments.of(GraduateLevel.Undergraduate, 0, 14, 1, MIN_ERR, 0, new ArrayList<>(){{add(0);}} ),
+                Arguments.of(GraduateLevel.Undergraduate, 0, 16, 0, MAX_ERR, 1, new ArrayList<>(){{add(MAX_CREDIT_FAILED);}}),
+                Arguments.of(GraduateLevel.Undergraduate, 18, 21, 0, MAX_ERR, 0, new ArrayList<>(){{add(MAX_CREDIT_FIRST_SEM);}}),
+                Arguments.of(GraduateLevel.Undergraduate, 16, 20, 0, 0, 0, new ArrayList<>()),
+                Arguments.of(GraduateLevel.Masters, 16, 12, 0, 0, 0, new ArrayList<>()),
+                Arguments.of(GraduateLevel.Undergraduate, 0, 20, 0, MAX_ERR, 1, new ArrayList<>(){{add(MAX_CREDIT_FAILED);}}),
+                Arguments.of(GraduateLevel.PHD, 18, 6, 0, MIN_ERR, 0, new ArrayList<>(){{add(0);}}),
+                Arguments.of(GraduateLevel.Masters, 20, 7, 0, MIN_ERR, 1, new ArrayList<>(){{add(0);}}),
+                Arguments.of(GraduateLevel.Undergraduate, 0, 21, 0, MAX_ERR, 1, new ArrayList<>(){{add(MAX_CREDIT_FIRST_SEM);}}),
+                Arguments.of(GraduateLevel.Masters, 20, 10, 0, MIN_ERR, 0, new ArrayList<>(){{add(0);}}),
+                Arguments.of(GraduateLevel.Undergraduate, 10, 15, 0, MAX_ERR, 1, new ArrayList<>(){{add(MAX_CREDIT_FAILED);}}),
+                Arguments.of(GraduateLevel.Undergraduate, 16, 21, 0, MAX_ERR, 1, new ArrayList<>(){{add(MAX_CREDIT_FIRST_SEM);}}),
+                Arguments.of(GraduateLevel.Undergraduate, 16, 28, 0, MAX_ERR, 2, new ArrayList<>(){{add(MAX_CREDIT_FIRST_SEM);add(24);}}),
+                Arguments.of(GraduateLevel.Masters, 16, 14, 0, MAX_ERR, 1, new ArrayList<>(){{add(12);}})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    public void limitTest(GraduateLevel gl, double gpa, int totalCredit, int totalCreditTaken, int cls, int errNum, ArrayList<Integer> exp_fail_max) throws Exception {
         Student stu = Mockito.mock(Student.class);
         when(stu.getGraduateLevel()).thenReturn(gl);
         when(stu.calculateGPA()).thenReturn(new Grade(gpa));
@@ -52,7 +58,7 @@ public class CheckValidGPALimitTest {
             totalCredit -= 1;
         }
 
-        el = new EnrollmentList("12345", stu);
+        EnrollmentList el = new EnrollmentList("12345", stu);
         el.addSections(
                 new Section(new Course("1234567", "AI", credits[0], "Masters"), "1"),
                 new Section(new Course("1234568", "AI", credits[1], "Masters"), "2"),
@@ -62,40 +68,15 @@ public class CheckValidGPALimitTest {
                 new Section(new Course("1234563", "AI", credits[5], "Masters"), "6"),
                 new Section(new Course("1234564", "AI", credits[6], "Masters"), "7")
         );
-        errorClass = cls;
-        expected_length = errNum;
-        exp_total = exp_fail_max;
-    }
-    @Parameters
-    public static Collection<Object[]> parameters() {
-        return Arrays.asList(new Object [][]{
-                {GraduateLevel.Undergraduate, 20, 10, 0, MIN_ERR, 1, new ArrayList<>(){{add(0);}} },
-                {GraduateLevel.Undergraduate, 0, 14, 1, MIN_ERR, 0, new ArrayList<>(){{add(0);}} },
-                {GraduateLevel.Undergraduate, 0, 16, 0, MAX_ERR, 1, new ArrayList<>(){{add(MAX_CREDIT_FAILED);}}},
-                {GraduateLevel.Undergraduate, 18, 21, 0, MAX_ERR, 0, new ArrayList<>(){{add(MAX_CREDIT_FIRST_SEM);}}},
-                {GraduateLevel.Undergraduate, 16, 20, 0, 0, 0, new ArrayList<>()},
-                {GraduateLevel.Masters, 16, 12, 0, 0, 0, new ArrayList<>()},
-                {GraduateLevel.Undergraduate, 0, 20, 0, MAX_ERR, 1, new ArrayList<>(){{add(MAX_CREDIT_FAILED);}}},
-                {GraduateLevel.PHD, 18, 6, 0, MIN_ERR, 0, new ArrayList<>(){{add(0);}}},
-                {GraduateLevel.Masters, 20, 7, 0, MIN_ERR, 1, new ArrayList<>(){{add(0);}}},
-                {GraduateLevel.Undergraduate, 0, 21, 0, MAX_ERR, 1, new ArrayList<>(){{add(MAX_CREDIT_FIRST_SEM);}}},
-                {GraduateLevel.Masters, 20, 10, 0, MIN_ERR, 0, new ArrayList<>(){{add(0);}}},
-                {GraduateLevel.Undergraduate, 10, 15, 0, MAX_ERR, 1, new ArrayList<>(){{add(MAX_CREDIT_FAILED);}}},
-                {GraduateLevel.Undergraduate, 16, 21, 0, MAX_ERR, 1, new ArrayList<>(){{add(MAX_CREDIT_FIRST_SEM);}}},
-                {GraduateLevel.Undergraduate, 16, 28, 0, MAX_ERR, 2, new ArrayList<>(){{add(MAX_CREDIT_FIRST_SEM);add(24);}}},
-                {GraduateLevel.Masters, 16, 14, 0, MAX_ERR, 1, new ArrayList<>(){{add(12);}}}
-        });
-    }
-    @Test
-    public void limitTest() {
+
         List<EnrollmentRuleViolation> evs = el.checkValidGPALimit();
-        assertEquals(expected_length, evs.size());
+        assertEquals(errNum, evs.size());
         int i = 0;
         for(EnrollmentRuleViolation v : evs) {
-            if (errorClass == MIN_ERR)
+            if (cls == MIN_ERR)
                 assertTrue(v instanceof MinCreditsRequiredNotMet);
             else
-                assertEquals(v.toString(), (new MaxCreditsLimitExceeded(exp_total.get(i))).toString());
+                assertEquals(v.toString(), (new MaxCreditsLimitExceeded(exp_fail_max.get(i))).toString());
             i++;
         }
     }
